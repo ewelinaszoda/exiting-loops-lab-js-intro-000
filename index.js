fcdfaab39370260866e15a9e6be854b98f50ba1c
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-23 22:28:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function keepGoing(array, changeValue, skipValue) {
   return array;
 }
 
-function findBy(array, findFn){
+function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++){
-    if(findFn(array[i])){
+    if(findFn(array[i])) {
       return array[i];
     }
   }
